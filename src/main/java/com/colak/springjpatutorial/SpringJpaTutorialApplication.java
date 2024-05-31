fad97ce513b5078ef0610c51c973b324ae70b582
Orçun Çolak <orcun.colak@hazelcast.com>
2024-05-31 10:22:58 +0300

    get person list

--- src/main/java/com/colak/springjpatutorial/SpringJpaTutorialApplication.java
+++ src/main/java/com/colak/springjpatutorial/SpringJpaTutorialApplication.java
@@ -30,28 +30,29 @@ public class SpringJpaTutorialApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        List<Person> list = new ArrayList<>();
-
-        {
-            Person person1 = new Person();
-            person1.setName("John");
-            LocalDateTime localDateTime1 = LocalDateTime.of(1976, 2, 6, 0, 0);
-            person1.setBirthDate(localDateTime1);
-            list.add(person1);
-        }
-
-        {
-            Person person2 = new Person();
-            person2.setName("Jane");
-            LocalDateTime localDateTime2 = LocalDateTime.of(2004, 11, 29, 0, 0);
-            person2.setBirthDate(localDateTime2);
-            list.add(person2);
-        }
+        List<Person> list = getPersonList();
 
         List<Person> savedPersonList = personService.create(list);
         log.info("Saved Person id : {}", savedPersonList);
 
         List<Person> byFormattedBirthDate = personService.findByBirthDateYear("1976");
         log.info("byFormattedBirthDate : {}", byFormattedBirthDate);
+    }
+
+    private static List<Person> getPersonList() {
+        List<Person> list = new ArrayList<>();
+
+        Person person1 = new Person();
+        person1.setName("John");
+        LocalDateTime localDateTime1 = LocalDateTime.of(1976, 2, 6, 0, 0);
+        person1.setBirthDate(localDateTime1);
+        list.add(person1);
+
+        Person person2 = new Person();
+        person2.setName("Jane");
+        LocalDateTime localDateTime2 = LocalDateTime.of(2004, 11, 29, 0, 0);
+        person2.setBirthDate(localDateTime2);
+        list.add(person2);
+        return list;
     }
 }
